71fd8457b4d4a95637d97d45e889f80747a67d8c
AndreasAchilleos81 <andreas.p.achilleos@gmail.com>
2025-05-03 16:36:13 +0300

    Blazor notes and FeedbackApp fixes

--- 4. blazor-for-front-end-development/tryOuts/FeedbackApp/Models/Feedback.cs
+++ 4. blazor-for-front-end-development/tryOuts/FeedbackApp/Models/Feedback.cs
@@ -1,3 +1,14 @@
-using System.ComponentModel.DataAnnotations;namespace FeedbackApp.Models;public class Feedback{    [Required(ErrorMessage = "Need your name")]    public string Name { get; set; }    [EmailAddress(ErrorMessage = "No email address no feedback")]    public string Email { get; set; }    [StringLength(maximumLength:500, MinimumLength = 10, ErrorMessage = "Enter between 10 and 500 chars")]
+using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackApp.Models;
+public class Feedback{
+    [Required(ErrorMessage = "Need your name")]
+    public string Name { get; set; }
+
+    [EmailAddress(ErrorMessage = "No email address no feedback")]
+    public string Email { get; set; }
+
+    [StringLength(maximumLength: 500, MinimumLength = 10, ErrorMessage = "Enter between 10 and 500 chars")]
     [Required]
-    public string Comment { get; set; }}
+    public string Comment { get; set; }
+}
