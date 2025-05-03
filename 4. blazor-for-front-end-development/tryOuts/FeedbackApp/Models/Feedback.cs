879dc51865929967a088d35994c0cf22a2f084a4
AndreasAchilleos81 <andreas.p.achilleos@gmail.com>
2025-05-03 10:55:39 +0300

    FeedbackForm with EditContext

--- 4. blazor-for-front-end-development/tryOuts/FeedbackApp/Models/Feedback.cs
+++ 4. blazor-for-front-end-development/tryOuts/FeedbackApp/Models/Feedback.cs
@@ -1,6 +1,3 @@
-namespace FeedbackApp.Models;
-public class Feedback{
-    public string Name { get; set; }
-    public string Email { get; set; }   
-    public string Comment { get; set; }
-}
+using System.ComponentModel.DataAnnotations;namespace FeedbackApp.Models;public class Feedback{    [Required(ErrorMessage = "Need your name")]    public string Name { get; set; }    [EmailAddress(ErrorMessage = "No email address no feedback")]    public string Email { get; set; }    [StringLength(maximumLength:500, MinimumLength = 10, ErrorMessage = "Enter between 10 and 500 chars")]
+    [Required]
+    public string Comment { get; set; }}
