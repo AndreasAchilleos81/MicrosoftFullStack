b57bec31c88705e6a111ea486aa91ed9fb85269b
Andreas <a.achilleos@fxpro.com>
2025-08-03 13:37:48 +0300

    part 2 of assignment

--- 5. Backend Development/tryouts/UserManagementAPI/Program.cs
+++ 5. Backend Development/tryouts/UserManagementAPI/Program.cs
@@ -1,89 +1,112 @@
 using System.Collections.Concurrent;
-using Microsoft.AspNetCore.Http.HttpResults;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using UserManagementAPI.Models;
 
 
-ConcurrentDictionary<int, User> users = new ConcurrentDictionary<int, User>();
-
-for (int i = 0; i < 10; i++)
+internal class Program
 {
-    var user = User.GenerateUser();
-    bool added = users.TryAdd(user.Id, user);
+    static ConcurrentDictionary<int, User> users = new ConcurrentDictionary<int, User>();
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var user = User.GenerateUser();
+            bool added = users.TryAdd(user.Id, user);
+        }
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection(); // TODO: setup https
+
+        app.Use(async  (context, next) =>
+        {
+            try
+            {
+                await next.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(context.Request.Path);
+                Console.WriteLine(ex.ToString());
+            }
+        });
+
+        app.MapGet("/", () => "I AM GET-ROOT");
+
+        app.MapGet("/users", () =>
+        {
+            return Results.Ok(users);
+        });
+
+        app.MapGet("/users/{id:int}", (int id) =>
+        {
+            User user = null;
+            if (users.TryGetValue(id, out user))
+            {
+                return Results.Ok(user);
+            }
+            return Results.NotFound();
+        });
+
+        app.MapPost("/users", ([FromBody] User user) =>
+        {
+            var validationResults = new List<ValidationResult>();
+            if (Validator.TryValidateObject(user, new ValidationContext(user), validationResults, true))
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (var issue in validationResults)
+                {
+                    stringBuilder.AppendLine(issue.ErrorMessage);
+                }
+
+                return Results.BadRequest(stringBuilder.ToString());
+            }
+            if (users.TryAdd(user.Id, user))
+            {
+                return Results.Ok($"Added user with Id: {user.Id}");
+            }
+            else
+            {
+                return Results.BadRequest($"Failed to add User with Id {user.Id}");
+            }
+        });
+
+
+        app.MapPut("/users", ([FromBody] User user) =>
+        {
+            var newId = user.Id;
+            var result = users.AddOrUpdate(newId, newId => user, (newId, existingUser) => user);
+            return Results.Ok($"User with Id:{result.Id} was updated");
+        });
+
+
+        app.MapDelete("/users/{id:int}", (int id) =>
+        {
+            var removed = users.TryRemove(id, out User user);
+            if (removed)
+            {
+                return Results.Ok($"Successfully removed user with Id: {user.Id}");
+            }
+            else
+            {
+                return Results.NotFound($"Removed user with Id: {id}");
+            }
+        });
+
+        app.Run();
+    }
 }
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection(); // TODO: setup https
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/", () => "I AM GET-ROOT");
-
-app.MapGet("/users", () =>
-{
-    return Results.Ok(users);
-});
-
-app.MapGet("/users/{id:int}", (int id) =>
-{
-    User user = null;
-    if (users.TryGetValue(id, out user))
-    {
-        return Results.Ok(user);
-    }
-    return Results.NotFound();
-});
-
-app.MapPost("/users", ([FromBody]User user) =>
-{
-    if (users.TryAdd(user.Id, user))
-    {
-        return Results.Ok($"Added user with Id: {user.Id}");
-    }
-    else
-    {
-        return Results.BadRequest($"Failed to add User with Id {user.Id}");
-    }
-});
-
-
-app.MapPut("/users", ([FromBody]User user) =>
-{
-    var newId = user.Id;
-    users.AddOrUpdate(newId, newId => user , (newId, existinUser) => user);  
-});
-
-
-app.MapDelete("/users/{id:int}", (int id)=>
-{
-    var removed = users.TryRemove(id, out User user);
-    if (removed)
-    {
-        Results.Ok($"Successfully removed user with Id: {user.Id}");
-    }
-    else
-    {
-        Results.NotFound($"Removed user with Id: {id}");
-    }
-});
-
-app.Run();
-
-
--- 5. Backend Development/tryouts/UserManagementAPI/readme.md
+++ 5. Backend Development/tryouts/UserManagementAPI/readme.md
@@ -3,3 +3,11 @@ Documenting for later usage how co-pilot helped me create this minimal api
 Scafolding was build easily and in a matter of seconds
 After scafolding I could add more interesting User properties with minimal changes to the api endpoins
 Co-pilot also helped me build fast my requests.http file
+
+
+
+My code was working just fine but co-pilot pointed out I was not returning my Results.Ok and Results.NotFound
+for my Put method
+It also pointed out some spelling mistakes for consistency
+And it also pointed out that I could validate my incoming objects.
+It suggested that I wrap some code it try catch blocks so I created a globla middelware to catch all exceptions
